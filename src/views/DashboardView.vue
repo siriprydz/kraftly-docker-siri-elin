7a1662ce7098b3647d0efd130378c72dc3c002e6
elinfelicia <elinratcliff@icloud.com>
2026-09-10 14:11:56 +0200

    optimise hero

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img src="../assets/hero.jpg" class="hero" alt="">
+    <img src="../assets/hero.webp" class="hero" alt="">
     <h1 v-if="userStore.user">Hej {{ userStore.user.name.split(' ')[0] }}!</h1>
     <h1 v-else>Hej!</h1>
 
